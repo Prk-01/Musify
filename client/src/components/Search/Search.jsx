690481cbb20112eed14c51ccbe89433ad90cb152
prk <prk.lsix@gmail.com>
2024-07-02 12:26:08 +0100

    Search Box component (UI changes )

--- client/src/components/Search/Search.jsx
+++ client/src/components/Search/Search.jsx
@@ -9,14 +9,20 @@ const Search = () => {
     //State to store fetched data
   const [input,setInput]=useState("");
   const [completions,setCompletions]=useState([]);
+  const [valuelength,setValuelength]=useState(0);
 
   //Function to fetch data from server
   const fetchData= async (value)=>{
+    setValuelength(value.length)
+    //If the input is not empty dont fetch data
     if (value.length > 0)
       {
       const response=await fetch('http://localhost:8000/autocomplete/'+value)
       const res=await response.json();
       setCompletions(res.results)
+    }
+    else{
+      setCompletions([])
     }
     
   }
@@ -31,7 +37,7 @@ const Search = () => {
     <div className="w-full">
         <div className="mx-auto">
         <input value={input} type="text" onChange={(e)=>handleChange(e.target.value)} className="mx-auto w-[80%] search" placeholder="Musify Here" />
-        <SearchBox completions={completions}/>
+        <SearchBox completions={completions} valuelength={valuelength}/>
         </div>
         
     </div>
--- client/src/components/Search/SearchBox.jsx
+++ client/src/components/Search/SearchBox.jsx
@@ -1,15 +1,21 @@
 //SearchBox component to display the search completions
 
 
-const SearchBox = ({ completions }) => {
+const SearchBox = ({ completions,valuelength }) => {
+
+    const handleChange=(e)=>{
+        console.log(e.target.id)
+    }
 
   return (
-    <div className="text-white w-[80%] mx-auto  md:mx-0 max-h-[300px] h-auto mt-1 overflow-y-scroll">
+    <div className="text-white w-[80%] mx-auto  md:mx-0 max-h-44 mt-1 overflow-y-auto custom-scrollbar">
         {
         /*Get the results and display */
             completions.map((completion,index)=>(
-                <div key={index} className="m-2 px-2 flex flex-col justify-center hover:bg-slate-600">
-                <span>{completion[0]}</span>
+                // lets rediret it the selected option to youtube for now
+                <div key={index} id={index} className=" bg-[#1A1A17] m-2 px-2 justify-center hover:bg-slate-600" onClick={(e)=>handleChange(e)}>
+                    {/* To highlight the search results */}
+                <span>{completion[0].slice(0,valuelength)}</span><span className="font-bold">{completion[0].slice(valuelength)}</span>
                 </div>
             ))
         }
