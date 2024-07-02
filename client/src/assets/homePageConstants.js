462b636eda7cdb8e16ffcb8dbd89c1bebe2e52c4
prk <prk.lsix@gmail.com>
2024-07-02 23:07:43 +0100

    Home Page (Type Animation)

---
+++ client/src/assets/homePageConstants.js
@@ -0,0 +1,6 @@
+//This file is a JavaScript file that contains the constants for the home page
+//Constants for typing animation
+
+export const typedConstants = ["choose the right track for your trek.",
+                        "Search by artist, album, or track.", 
+                        "Discover new music."];
--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -2,6 +2,8 @@
 // The Home component will contain a search bar and an image.
 
 import Search from "./Search/Search"
+import { ReactTyped } from "react-typed";
+import { typedConstants } from "../assets/homePageConstants";
 
 // The search bar will be used to search for tracks.
 const Home = () => {
@@ -11,7 +13,11 @@ const Home = () => {
         <div className="max-w-[1440px] w-full mt-[-96px] h-screen mx-auto pt-[30vh]">
             <div className="w-full mx-auto grid md:grid-cols-2 gap-6 px-4">
                 <div className="p-0 md:pl-12 h-auto md:h-[50vh] text-center md:text-justify">
-                    <h1 className="md:text-3xl sm:text-2xl text-xl mb-6">Life is a journey,<br /> choose the right track for your trek.</h1>
+                    <h1 className="md:text-3xl sm:text-2xl text-xl mb-6">Life is a journey,
+                    {/* React Typing Animation */}
+                    <br />
+                    <ReactTyped strings={typedConstants} typeSpeed={80} backSpeed={80} loop />
+                    </h1>
                     {/* Input Component needs to be added here */}
                     <Search />
                 </div>  
