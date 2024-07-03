65c66cbfcc251eac89ed0d47469bd34b3df3c7a1
prk <prk.lsix@gmail.com>
2024-07-03 23:33:13 +0100

    Code Documentation

--- server/autocomplete/trie_structure.py
+++ server/autocomplete/trie_structure.py
@@ -26,13 +26,15 @@ class Trie:
         curr.word = True
 
     # Searches for a word in the trie
-    def search(self, word):
-        curr = self.root
-        for c in word:
-            if c not in curr.children:
-                return False
-            curr = curr.children[c]
-        return curr.word
+    # Not used handled in front end
+    #feature not used can be used to validate exact match
+    # def search(self, word):
+    #     curr = self.root
+    #     for c in word:
+    #         if c not in curr.children:
+    #             return False
+    #         curr = curr.children[c]
+    #     return curr.word
 
     # Finds the node corresponding to the given prefix
     def _find_node(self, prefix):
@@ -57,11 +59,11 @@ class Trie:
     
     #Not used handled in front end
     # Returns True if the given prefix is valid (i.e., there is a word in the trie that starts with the prefix)
-    def is_valid(self, prefix):
-        node = self._find_node(prefix)
-        if not node:
-            return False
-        return True
+    # def is_valid(self, prefix):
+    #     node = self._find_node(prefix)
+    #     if not node:
+    #         return False
+    #     return True
     
 
     # Returns a list of words in the trie that start with the given prefix
